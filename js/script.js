e69060d4924748348d8b600772884b5d85af9e4f
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-01-15 22:17:33 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,94 @@
 
+
+//DOM
+
+//console.log(navigator.platform);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//===============================================
+//==============================================
+
+/*const block = document.querySelector('ul>li');
+
+
+block.onclick = scrollIntoView;
+
+function scrollIntoView() {
+	const sec = document.querySelector('section');
+	sec.scrollIntoView({
+		block: "center",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+
+
+function aniScroll() {
+	window.scrollTo({
+		top: 1550,
+		left: 0,
+		behavior: "smooth"
+	});
+};
+setTimeout(aniScroll, 1000);
+
+let scrollWidth = Math.max(
+	document.body.scrollWidth, document.documentElement.scrollWidth,
+	document.body.offsetWidth, document.documentElement.offsetWidth,
+	document.body.clientWidth, document.documentElement.clientWidth,
+);
+
+let scrollHeight = Math.max(
+	document.body.scrollHeight, document.documentElement.scrollHeight,
+	document.body.offsetHeight, document.documentElement.offsetHeight,
+	document.body.clientHeight, document.documentElement.clientHeight,
+);
+console.log(scrollWidth);
+console.log(scrollHeight);
+
+*/
+//const button = document.querySelector('.button');
+//const firstcon = document.querySelector('.content');
+
+/*button.addEventListener("click", event => {
+	firstcon.classList.toggle('click');
+});*/
+/*
+document.addEventListener("click", function (event) {
+	if (event.target.closest('.button')) {
+		firstcon.classList.toggle('click');
+	} else if (!event.target.closest('.button')) {
+		firstcon.classList.remove('click');
+	}
+})*/
+
 console.log(navigator.platform)
 const app = document.querySelector('.nav__app');
 if (navigator.platform === 'Win32' || navigator.platform === 'Win64') {
@@ -81,7 +171,7 @@ function removeDie() {
 	else {
 		if (arr.length === 4) {
 			setTimeout(function () {
-				window.navigator.vibrate([200, 200, 200]);
+				vibrate();
 				arr.splice(0, arr.length);
 				circleArray.splice(0, circleArray.length);
 				circleFirst.classList.remove('yellow');
@@ -120,6 +210,13 @@ lock.addEventListener("click", function (event) {
 	}
 });
 
-function sound() {
-	audio.play();
+console.log(navigator.userAgent);
+
+function vibrate() {
+	if (window.navigator && window.navigator.vibrate) {
+		window.navigator.vibrate([200, 200, 200]);
+	} else {
+		console.log('Не поддерживается');
+	}
+
 }
